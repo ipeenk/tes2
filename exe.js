be0126faad728f920c7951acf7a1a2effc54f7f4
ipeenk <30693240+ipeenk@users.noreply.github.com>
2020-02-08 14:50:55 +0700

    Add files via upload

---
+++ exe.js
@@ -0,0 +1,7 @@
+<script type="text/javascript" target="amp-script">
+    var app_url = 'https://exe.io/';
+    var app_api_token = 'e06657c446af9cb26fed80a3f100eb32df69221c';
+    var app_advert = 2;
+    var app_exclude_domains = ["depositfiles.com"];
+</script>
+<script src='//exe.io/js/full-page-script.js'></script>

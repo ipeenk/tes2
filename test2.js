116384d71e8f5d42d972cf4b068799e7bd2bc817
ipeenk <30693240+ipeenk@users.noreply.github.com>
2020-02-08 14:48:48 +0700

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,6 @@
+<script id="hello-world" type="text/plain" target="amp-script">
+  const btn = document.querySelector('button');
+  btn.addEventListener('click', () => {
+    document.body.textContent = 'Hello World!';
+  });
+</script>
